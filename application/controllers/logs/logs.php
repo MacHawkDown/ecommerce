f91f2cf73e9cbd4824d32af412c4298e1b1d31b7
hareesh <hareesh.puliyedath@gmail.com>
2015-05-06 18:51:27 -0700

    template for the login view

--- application/controllers/logs/logs.php
+++ application/controllers/logs/logs.php
@@ -1,46 +1,43 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Logs extends CI_Controller {
+    class Logs extends CI_Controller {
 
-	public function __construct()
-	{
-		parent::__construct();
-		// $this->output->enable_profiler();
-		$this->load->model('Log');
-	}
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->model('Log');
+        }
 
-	public function index()
-	{
-		//$this->load->view('index');
-		$this->load->view('dashboard/header_search', array('control' => ''));
-	}
-		public function log_in()
-	{
-		$log = $this->Log->get_user_email($this->input->post('email'));
-		if($this->input->post('email') === NULL)
-		{
-			$this->session->set_flashdata('login_error', 'Wrong email');
-			redirect("/");
-		}
-		$this->load->helper(array("form","url"));
-		$this->load->library("form_validation");
-		$this->form_validation->set_rules("email", "Email", "trim|required");
-		$this->form_validation->set_rules("password", "Password", "trim|required");
-		if(count($log) < 1) {
-			$this->session->set_flashdata('login_error', 'This email is not in the database');
-			redirect("/");
-		}
-		elseif($this->form_validation->run() === FALSE || $log['password'] != set_value('password'))
-		{
-			$this->session->set_flashdata('login_error', 'Wrong password');
-			redirect("/");
-		}
-		else
-		{
-			$this->load->view('/logs/welcome', array('first_name'=>$log['first_name'],'last_name'=>$log['last_name'],'email'=>$log['email']));
-		}
-	}
+        public function index()
+        {
+            $this->load->view('dashboard/header_search', array('control' => ''));
+        }
+        public function log_in()
+        {
+            $log = $this->Log->get_user_email($this->input->post('email'));
+            if($this->input->post('email') === NULL)
+            {
+                $this->session->set_flashdata('login_error', 'Wrong email');
+                redirect("/");
+            }
+            $this->load->helper(array("form","url"));
+            $this->load->library("form_validation");
+            $this->form_validation->set_rules("email", "Email", "trim|required");
+            $this->form_validation->set_rules("password", "Password", "trim|required");
+            if(count($log) < 1) {
+                $this->session->set_flashdata('login_error', 'This email is not in the database');
+                redirect("/");
+            }
+            elseif($this->form_validation->run() === FALSE || $log['password'] != set_value('password'))
+            {
+                $this->session->set_flashdata('login_error', 'Wrong password');
+                redirect("/");
+            }
+            else
+            {
+                $this->load->view('/logs/welcome', array('first_name'=>$log['first_name'],'last_name'=>$log['last_name'],'email'=>$log['email']));
+            }
+        }
 
-}
-
-//end of main controller
+    }
+    //end of main controller
